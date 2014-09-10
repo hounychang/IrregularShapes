c3d573cae7c73b52a30ef2488ab62847d5760584
Houny <hounychang@gmail.com>
2014-09-10 17:28:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+##INFO
+This Project is base on [Styling Android's](http://blog.stylingandroid.com/) series named `Irregular Shapes`.
+I just write all code in one Project.
+
+In this series,there are: 
+
+---
+1. [Par 1](http://blog.stylingandroid.com/irregular-shapes-part-1/) 
+2. [Par 2](http://blog.stylingandroid.com/irregular-shapes-part-2/) 
+3. [Par 3](http://blog.stylingandroid.com/irregular-shapes-part-3/) 
+4. [Par 4](http://blog.stylingandroid.com/irregular-shapes-part-4/) 
+
+---
+
+##Thanks
+***Author: [MarkAllison](https://plus.google.com/+MarkAllison/posts)***
+
+***Blog: [Styling Android](http://blog.stylingandroid.com/)***
+
+
+
+##中文介绍
+看了[Styling Android](http://blog.stylingandroid.com/)发表`Irregular Shapes`系列教程之后，整理除了这个工程。
+  该系列教程共有四篇，分别是：
+
+
+---
+1. [Par 1](http://blog.stylingandroid.com/irregular-shapes-part-1/) 
+2. [Par 2](http://blog.stylingandroid.com/irregular-shapes-part-2/) 
+3. [Par 3](http://blog.stylingandroid.com/irregular-shapes-part-3/) 
+4. [Par 4](http://blog.stylingandroid.com/irregular-shapes-part-4/) 
+
+---
+
+##致谢
+***文章作者：[MarkAllison](https://plus.google.com/+MarkAllison/posts)***
+
+***博客：[Styling Android](http://blog.stylingandroid.com/)***
+
+
+#License
+© 2014, [MarkAllison](https://plus.google.com/+MarkAllison/posts). All rights reserved. This article originally appeared on [Styling Android](http://blog.stylingandroid.com/).
+> Portions of this page are modifications based on work created and shared by Google and used according to terms described in the Creative Commons 3.0 Attribution License
+
---
+++ app/src/main/java/com/houny/irregularshapes/MyActivity.java
@@ -0,0 +1,160 @@
+package com.houny.irregularshapes;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Locale;
+
+
+public class MyActivity extends ActionBarActivity implements ActionBar.TabListener {
+
+
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+
+    ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return getString(R.string.title_section1).toUpperCase(l);
+                case 1:
+                    return getString(R.string.title_section2).toUpperCase(l);
+                case 2:
+                    return getString(R.string.title_section3).toUpperCase(l);
+                case 3:
+                    return getString(R.string.title_section4).toUpperCase(l);
+            }
+            return null;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+            return rootView;
+        }
+    }
+
+}
